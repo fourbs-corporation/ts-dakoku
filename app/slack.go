5fd5665df413df53d4254816cdcf88578d72ac36
Fourbs <83324044+fourbs-corporation@users.noreply.github.com>
2022-02-24 11:28:59 +0900

    エラーのログ出力

--- app/slack.go
+++ app/slack.go
@@ -74,6 +74,7 @@ func (ctx *Context) getActionCallback(data *slack.AttachmentActionCallback) (*sl
 		params.ResponseType = "ephemeral"
 		params.ReplaceOriginal = false
 		params.Text = "勤務表の更新に失敗しました :warning:"
+		log.Printf(err)
 	}
 
 	return params, data.ResponseURL, nil
